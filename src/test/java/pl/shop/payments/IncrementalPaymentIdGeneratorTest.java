6b9d24f3e7a304b5eef48a55245b00ceab7a5d5f
Robert Burek <robertburek@wp.pl>
2020-11-08 13:09:04 +0100

    Test IncrementalPaymentIdGenerator, metody getNext()
      test sprawdzający poprawność inkrementacji id

--- src/test/java/pl/shop/payments/IncrementalPaymentIdGeneratorTest.java
+++ src/test/java/pl/shop/payments/IncrementalPaymentIdGeneratorTest.java
@@ -3,6 +3,7 @@ package pl.shop.payments;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Long.parseLong;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IncrementalPaymentIdGeneratorTest {
@@ -16,6 +17,14 @@ class IncrementalPaymentIdGeneratorTest {
     void shouldGenerateValidId() {
         String id = incrementalPaymentIdGenerator.getNext();
         assertEquals(true, id.matches(ID_FORMAT));
+    }
+
+    @DisplayName("Should generate Id by incrementing value of previousOne")
+    @Test
+    void shouldGenerateIdByIncrementingValueOfPreviousOne() {
+        long firstValue = parseLong(incrementalPaymentIdGenerator.getNext());
+        long secondValue = parseLong(incrementalPaymentIdGenerator.getNext());
+        assertEquals(firstValue + 1, secondValue);
     }
 
 }
